043e164a13e0912565e537a6bc9249cf08442754
Jesús A. Rodríguez <162174+fresus@users.noreply.github.com>
2025-03-31 10:53:45 +0200

    Update astronvim to v5

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -6,13 +8,5 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.ansible" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.cue" },
-  { import = "astrocommunity.pack.helm" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.terraform" },
-  { import = "astrocommunity.pack.yaml" },
   -- import/override with your plugins folder
 }
